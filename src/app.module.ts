64fcc8d6ff8f6608489005a1e19b9f02a96373e2
ian <ianiqball@gmail.com>
2026-01-21 14:44:24 +0700

    feat: search api done

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { ConfigModule } from '@nestjs/config';
 import { SeriesModule } from './series/series.module';
+import { GeneralModule } from './general/general.module';
 @Module({
-  imports: [MoviesModule,ConfigModule.forRoot(), SeriesModule] ,
+  imports: [MoviesModule,ConfigModule.forRoot(), SeriesModule, GeneralModule] ,
   controllers: [AppController],
   providers: [AppService],
 })
